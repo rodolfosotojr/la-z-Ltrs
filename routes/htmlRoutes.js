ee5d74da4efab5b83f6fd43120d2ab328bb377c5
Orlando Carnate <orlando.carnate@gmail.com>
2019-03-26 21:06:22 -0500

    Routes adjustment

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,22 +5,22 @@ module.exports = function (app) {
 
   // Basic routes to the log in page
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../public/html/login.html"));
   });
 
   //Route to the home page
   app.get("/home", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/Home.html"));
+    res.sendFile(path.join(__dirname, "../public/html/Home.html"));
   });
 
   //Route to the input page
   app.get("/input", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/newLetter.html"));
+    res.sendFile(path.join(__dirname, "../public/html/newLetter.html"));
   });
 
   //Route to the how to guide
   app.get("/add", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/howTo.html"));
+    res.sendFile(path.join(__dirname, "../public/html/howTo.html"));
   });
 
   // Displays all of the past orders
